1b36570d4858cf0aea57e8c49353ed23534bcea4
santhoshparamasivam <santhoshparamasivam78@gmail.com>
2025-07-10 18:46:05 +0530

    updated server

--- node_server/index.cjs
+++ node_server/index.cjs
@@ -1,16 +1,33 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+
+dotenv.config()
+
 const supabase = require('@supabase/supabase-js')
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+
+const conn = supabase.createClient(supabaseUrl, supabaseKey)
 
 const PORT = 8080
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     console.log('hello')
 })
 
-app.post('/oauth_signin', (req, res) => {
-    console.log(req.body)
-    
+app.post('/oauth_signin', async (req, res) => {
+    const { data, error } = await conn.from('users').select('*')
+
+    if (error) {
+        res.send(error)
+        return
+    }
+
+    if (data) {
+        res.send(data)
+        return
+    }
 })
 
 app.listen(PORT, () => {
